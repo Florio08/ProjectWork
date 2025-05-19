e533e44b201ef019670ac4bca820b0befe558286
Florio08 <francescoflorio759@gmail.com>
2025-05-19 18:50:15 +0200

    aggiunta models views

---
+++ View Models/FormulaItemViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectWork1._0.ViewModels
+{
+    public class FormulaItemViewModel : BaseViewModel
+    {
+        private bool _isDescriptionVisible;
+
+        public string Titolo { get; set; }
+        public string Descrizione { get; set; }
+        public bool IsDescriptionVisible
+        {
+            get => _isDescriptionVisible;
+            set => SetProperty(ref _isDescriptionVisible, value);
+        }
+    }
+}
+
---
+++ View Models/FormuleViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
+
+namespace ProjectWork1._0.ViewModels
+{
+    public class FormuleViewModel : BaseViewModel
+    {
+        public ObservableCollection<FormulaItemViewModel> Formule { get; set; }
+        public ICommand ToggleDescriptionCommand { get; }
+
+        public FormuleViewModel()
+        {
+            Formule = new ObservableCollection<FormulaItemViewModel>
+            {
+                new FormulaItemViewModel { Titolo = "Formula 1", Descrizione = "Descrizione della formula 1" },
+                new FormulaItemViewModel { Titolo = "Formula 2", Descrizione = "Descrizione della formula 2" }
+            };
+
+            ToggleDescriptionCommand = new Command<FormulaItemViewModel>(ToggleDescription);
+        }
+
+        private void ToggleDescription(FormulaItemViewModel formula)
+        {
+            if (formula != null)
+                formula.IsDescriptionVisible = !formula.IsDescriptionVisible;
+        }
+    }
+}
+
